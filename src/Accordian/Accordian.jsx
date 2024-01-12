5185acbe21935c514b5e946e52e53049a543ca73
vinay2955 <vinaypatnala29@gmail.com>
2024-01-12 07:03:56 -0700

    new project

---
+++ src/Accordian/Accordian.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import data from "./data";
+import "./style.css";
+
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, SetMultipe] = useState([]);
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMultiSelection(getCurrentId) {
+    let cpyMultipe = [...multiple];
+    const findIndexOfCurrentId = cpyMultipe.indexOf(getCurrentId);
+    if (findIndexOfCurrentId === -1) {
+      cpyMultipe.push(getCurrentId);
+    } else {
+      cpyMultipe.splice(getCurrentId);
+    }
+    SetMultipe(cpyMultipe);
+  }
+  return (
+    <div className="acc-wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="acc-content ">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="acc-content ">{dataItem.answer}</div>
+                  )}
+              {/* {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null} */}
+            </div>
+          ))
+        ) : (
+          <div>No data found !</div>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 //import Login from './Login/Login';
-import TshirtsComponent from './Tshits/TshirtsComponent';
+//import TshirtsComponent from './Tshits/TshirtsComponent';
 //import TshirtSubChild from './Tshits/TshirtSubChild';
+import Accordian from './Accordian/Accordian';
 function App() {
   return (
     <div>
-     <TshirtsComponent/>
+     <Accordian/>
     </div>
   );
 }
